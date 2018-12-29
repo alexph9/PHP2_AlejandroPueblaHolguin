70eb9525fc7bdf61022096f13da150e2f9ecb34d
alexph9 <alexph9@hotmail.com>
2018-12-29 20:44:24 +0100

    First commit

---
+++ tests/Entity/ResultTest.php
@@ -0,0 +1,154 @@
+<?php
+/**
+ * PHP version 7.2
+ * tests/Entity/ResultTest.php
+ *
+ * @category EntityTests
+ * @package  MiW\Results\Tests
+ * @author   Javier Gil <franciscojavier.gil@upm.es>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://www.etsisi.upm.es ETS de Ingeniería de Sistemas Informáticos
+ */
+
+namespace MiW\Results\Tests\Entity;
+
+use MiW\Results\Entity\Result;
+use MiW\Results\Entity\User;
+
+/**
+ * Class ResultTest
+ *
+ * @package MiW\Results\Tests\Entity
+ */
+class ResultTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var User $user
+     */
+    private $user;
+
+    /**
+     * @var Result $result
+     */
+    private $result;
+
+    private const USERNAME = 'uSeR ñ¿?Ñ';
+    private const POINTS = 2018;
+
+    /**
+     * @var \DateTime $time
+     */
+    private $time;
+
+    /**
+     * Sets up the fixture.
+     * This method is called before a test is executed.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        $this->user = new User();
+        $this->user->setUsername(self::USERNAME);
+        $this->time = new \DateTime('now');
+        $this->result = new Result(
+            self::POINTS,
+            $this->user,
+            $this->time
+        );
+    }
+
+    /**
+     * Implement testConstructor
+     *
+     * @covers \MiW\Results\Entity\Result::__construct()
+     * @covers \MiW\Results\Entity\Result::getId()
+     * @covers \MiW\Results\Entity\Result::getResult()
+     * @covers \MiW\Results\Entity\Result::getUser()
+     * @covers \MiW\Results\Entity\Result::getTime()
+     *
+     * @return void
+     */
+    public function testConstructor(): void
+    {
+        self::assertEquals(self::USERNAME, $this->result->getUser());
+        self::assertEquals(self::POINTS, $this->result->getResult());
+    }
+
+    /**
+     * Implement testGet_Id().
+     *
+     * @covers \MiW\Results\Entity\Result::getId()
+     * @return void
+     */
+    public function testGetId():void
+    {
+        self::assertEquals(0, $this->result->getId());
+    }
+
+    /**
+     * Implement testUsername().
+     *
+     * @covers \MiW\Results\Entity\Result::setResult
+     * @covers \MiW\Results\Entity\Result::getResult
+     * @return void
+     */
+    public function testResult(): void
+    {
+        $this->result->setResult(2019);
+        self::assertEquals(2019, $this->result->getResult());
+    }
+
+    /**
+     * Implement testUser().
+     *
+     * @covers \MiW\Results\Entity\Result::setUser()
+     * @covers \MiW\Results\Entity\Result::getUser()
+     * @return void
+     */
+    public function testUser(): void
+    {
+        $this->user->setUsername(self::USERNAME.'alex');
+        self::assertEquals(self::USERNAME.'alex', $this->result->getUser());
+    }
+
+    /**
+     * Implement testTime().
+     *
+     * @covers \MiW\Results\Entity\Result::setTime
+     * @covers \MiW\Results\Entity\Result::getTime
+     * @return void
+     */
+    public function testTime(): void
+    {
+        $date = '2011-08-13 13:25:00';
+        $deteDT = \DateTime::createFromFormat('Y-m-d H:i:s', $date);
+        $this->result->setTime($deteDT);
+        self::assertEquals($deteDT, $this->result->getTime());
+    }
+
+    /**
+     * Implement testTo_String().
+     *
+     * @covers \MiW\Results\Entity\Result::__toString
+     * @return void
+     */
+    public function testToString(): void
+    {
+        $toString = $this->result->__toString();
+        $result = strpos($toString, 'alex');
+        self::assertFalse($result);
+    }
+
+    /**
+     * Implement testJson_Serialize().
+     *
+     * @covers \MiW\Results\Entity\Result::jsonSerialize
+     * @return void
+     */
+    public function testJsonSerialize(): void
+    {
+        $jsonSerialize = $this->result->jsonSerialize();
+        self::assertTrue(is_array($jsonSerialize));
+    }
+}
---
+++ tests/Entity/UserTest.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * PHP version 7.2
+ * tests/Entity/UserTest.php
+ *
+ * @category EntityTests
+ * @package  MiW\Results\Tests
+ * @author   Javier Gil <franciscojavier.gil@upm.es>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://www.etsisi.upm.es ETS de Ingeniería de Sistemas Informáticos
+ */
+
+namespace MiW\Results\Tests\Entity;
+
+use MiW\Results\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class UserTest
+ *
+ * @package MiW\Results\Tests\Entity
+ * @group   users
+ */
+class UserTest extends TestCase
+{
+    /**
+     * @var User $user
+     */
+    private $user;
+    /**
+     * Sets up the fixture.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->user = new User('alex', 'alex@xyz.com', 'alex');
+    }
+    /**
+     * @covers \MiW\Results\Entity\User::__construct()
+     */
+    public function testConstructor(): void
+    {
+        self::assertEquals('alex', $this->user->getUsername());
+        self::assertEquals('alex@xyz.com', $this->user->getEmail());
+        self::assertEquals(true, $this->user->validatePassword('alex'));
+    }
+    /**
+     * @covers \MiW\Results\Entity\User::getId()
+     */
+    public function testGetId(): void
+    {
+        self::assertEquals(0, $this->user->getId());
+    }
+    /**
+     * @covers \MiW\Results\Entity\User::setUsername()
+     * @covers \MiW\Results\Entity\User::getUsername()
+     */
+    public function testGetSetUsername(): void
+    {
+        $this->user->setUsername('alejandro');
+        self::assertEquals('alejandro', $this->user->getUsername());
+    }
+    /**
+     * @covers \MiW\Results\Entity\User::getEmail()
+     * @covers \MiW\Results\Entity\User::setEmail()
+     */
+    public function testGetSetEmail(): void
+    {
+        $this->user->setEmail('alejandro@xyz.com');
+        self::assertEquals('alejandro@xyz.com', $this->user->getEmail());
+    }
+    /**
+     * @covers \MiW\Results\Entity\User::setEnabled()
+     * @covers \MiW\Results\Entity\User::isEnabled()
+     */
+    public function testIsSetEnabled(): void
+    {
+        $this->user->setEnabled(true);
+        self::assertEquals(true, $this->user->isEnabled());
+    }
+    /**
+     * @covers \MiW\Results\Entity\User::setIsAdmin()
+     * @covers \MiW\Results\Entity\User::isAdmin
+     */
+    public function testIsSetAdmin(): void
+    {
+        $this->user->setIsAdmin(true);
+        self::assertEquals(true, $this->user->isAdmin());
+    }
+    /**
+     * @covers \MiW\Results\Entity\User::setPassword()
+     * @covers \MiW\Results\Entity\User::validatePassword()
+     */
+    public function testSetValidatePassword(): void
+    {
+        self::assertEquals(true, $this->user->validatePassword('alex'));
+    }
+    /**
+     * @covers \MiW\Results\Entity\User::__toString()
+     */
+    public function testToString(): void
+    {
+        self::assertEquals('alex', $this->user->__toString());
+    }
+    /**
+     * @covers \MiW\Results\Entity\User::jsonSerialize()
+     */
+    public function testJsonSerialize(): void
+    {
+        $jsonSerialize = $this->user->jsonSerialize();
+        self::assertTrue(is_array($jsonSerialize));
+    }
+}
